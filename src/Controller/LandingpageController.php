73a01dabda20865ca40e6d51263768c9588e0ef1
pchr-srf <philipp.christen@srf.ch>
2019-03-22 18:13:30 +0100

    return proxied image

--- src/Controller/LandingpageController.php
+++ src/Controller/LandingpageController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class LandingpageController extends AbstractController
 {
@@ -35,5 +36,16 @@ class LandingpageController extends AbstractController
         $data = json_decode($response->getContent(), true);
 
         return $this->json($data, 200, ['Access-Control-Allow-Origin' => '*']);
+    }
+
+    /**
+     * @Route("/image/{hash}/{size}", name="image")
+     */
+    public function image($hash, $size = '320ws')
+    {
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://www.srf.ch/static/cms/images/' . $size . '/' . $hash);
+
+        return new Response($response->getContent(), 200, ['Access-Control-Allow-Origin' => '*', 'Content-Type' => 'image/png']);
     }
 }
